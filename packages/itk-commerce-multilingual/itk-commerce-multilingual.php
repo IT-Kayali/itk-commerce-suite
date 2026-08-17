44bfcfa76a6383fd9135d38fbe9bd81fcddd6009
IT-Kayali <g.kayali@it-kayali.de>
2026-08-17 23:16:25 +0200

    Boot order language rendering scope

--- packages/itk-commerce-multilingual/itk-commerce-multilingual.php
+++ packages/itk-commerce-multilingual/itk-commerce-multilingual.php
@@ -48,6 +48,7 @@ function prepare() {
     require_once PATH . '/src/TranslationRepository.php';
     require_once PATH . '/src/TranslationWorkflow.php';
     require_once PATH . '/src/WooCommerceLanguageContext.php';
+    require_once PATH . '/src/OrderLanguageScope.php';
     require_once PATH . '/src/WooCommerceTranslationMapper.php';
     require_once PATH . '/src/MultilingualModule.php';
 
